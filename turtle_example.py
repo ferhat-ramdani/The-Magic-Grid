79bfd60a1387025795a022d1c531623f5d87457d
golderRatio <83746556+golderRatio@users.noreply.github.com>
2022-12-15 21:00:25 +0100

    basic files

---
+++ turtle_example.py
@@ -0,0 +1,77 @@
+import turtle as tur
+# import colorsys as cs
+# tur.setup(800, 800)
+# tur.speed(0)
+# tur.tracer(10)
+# tur.width(2)
+# tur.bgcolor("black")
+# for j in range(25):
+#     for i in range(15):
+#         tur.color(cs.hsv_to_rgb(i/15, j/25, 1))
+#         tur.right(90)
+#         tur.circle(200-j*4, 90)
+#         tur.left(90)
+#         tur.circle(200-j*4, 90)
+#         tur.right(180)
+#         tur.circle(50, 24)
+# tur.hideturtle()
+# tur.done()
+
+
+# tur.setup(400, 400)
+
+# tur.width(5)
+# tur.speed(1)
+# tur.forward(100)
+
+# tur.up()
+# tur.goto(0, -20)
+
+# tur.down()
+# tur.width(4)
+# tur.speed(1)
+# tur.forward(100)
+
+# tur.up()
+# tur.goto(0, -40)
+
+# tur.down()
+# tur.width(3)
+# tur.speed(1)
+# tur.forward(100)
+
+# tur.up()
+# tur.goto(0, -60)
+
+# tur.down()
+# tur.width(2)
+# tur.speed(1)
+# tur.forward(100)
+
+# tur.up()
+# tur.goto(0, -80)
+
+# tur.down()
+# tur.width(1)
+# tur.speed(1)
+# tur.forward(100)
+
+
+tur.width(5)
+tur.speed(1)
+tur.forward(50)
+
+tur.left(90)
+tur.width(2)
+tur.forward(50)
+
+tur.left(90)
+tur.width(4)
+tur.forward(50)
+
+tur.left(90)
+tur.width(1)
+tur.forward(50)
+
+
+tur.done()
