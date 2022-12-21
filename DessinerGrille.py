509d061229608a6d59de93ecf82afc673a703333
golderRatio <83746556+golderRatio@users.noreply.github.com>
2022-12-21 21:16:59 +0100

    stashed changes
    Plus Cours Chemin
    modification de Dessiner Grille

--- DessinerGrille.py
+++ DessinerGrille.py
@@ -52,7 +52,30 @@ def colorer_mur(ep):
         t.color("purple")
     
 
-def dessiner_grille(grille, n, m, pas):
+
+
+def ajoute_coor(n, m, pas):
+    t.color("red")
+    t.up()
+    t.goto(-300 + pas/2, 300 - pas/2)
+    t.left(90)
+    for l in range(1, n+1):
+        for c in range(1, m+1):
+            pos = "(" + str(l) + "," + str(c) + ")"
+            t.write(pos , move=False,align='center',font=('Arial',8,'bold'))
+            t.forward(pas)
+        t.backward((m)*pas)
+        t.right(90)
+        t.forward(pas)
+        t.left(90)
+
+
+
+
+
+
+def dessiner_grille(n, m, pas):
+    grille = Grille.creer_grille(n, m)
     #dessiner les murs horizontaux
     for l in range(1, n+2):
         for c in range(1, m+1):
@@ -85,32 +108,14 @@ def dessiner_grille(grille, n, m, pas):
         t.right(90)
         t.down()
     t.up()
-
-
-def ajoute_coor(n, m, pas):
-    t.color("red")
-    t.up()
-    t.goto(-300 + pas/2, 300 - pas/2)
-    t.left(90)
-    for l in range(1, n+1):
-        for c in range(1, m+1):
-            pos = "(" + str(l) + "," + str(c) + ")"
-            t.write(pos , move=False,align='center',font=('Arial',8,'bold'))
-            t.forward(pas)
-            print(l, c)
-        t.backward((m)*pas)
-        t.right(90)
-        t.forward(pas)
-        t.left(90)
-
+    ajoute_coor(n, m, pas)
 
 
 
 #____________partie_tests___________________
 # print( ep_mur(grille, (1, 5), 'v', 3, 4) )
-# gr = Grille.creer_grille(3, 4)
-# dessiner_grille(gr, 3, 4, 70)
 # ajoute_coor(3, 4, 70)
+dessiner_grille(7, 7, 70)
 #______________end_tests___________________
 
 t.color("white")
